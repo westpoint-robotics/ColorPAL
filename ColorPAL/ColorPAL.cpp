acbf0d10859a79a746b693c34fd84aaf7c226e84
Pratheek <pratheek.manjunath@westpoint.edu>
2019-12-03 19:21:26 -0500

    Rectified timing issues with serin port.
    1-Changed delay inside readPAL. 2-Changed serin.end().

--- ColorPAL/ColorPAL.cpp
+++ ColorPAL/ColorPAL.cpp
@@ -6,7 +6,7 @@ The Circuit:
 	+5v and gound from Arduino to sensor
 	Sensor Signal pin connected to any digital pin defined by the user.
 	
-By Pratheek Manjunath. November 12, 2019.
+By Pratheek Manjunath. November 19, 2019.
 */
 #include "ColorPAL.h"
 
@@ -15,19 +15,19 @@ void ColorPAL::attachPAL(int userPin)
   pin = userPin;
   serin = SoftwareSerial(pin, 255);
   serout = SoftwareSerial(255, pin);
-  delay(2000);                    	// Soft start required for ColorPAL
-  Serial.begin(9600);             	// Open Serial port for communication between Arduino and computer
+  delay(1000);                    	// Soft start required for ColorPAL
   resetPAL();                     	// Send reset to ColorPal
   serout.begin(4800);           	// Open serial port for Arduino to transmit to ColorPAL
   pinMode(pin, OUTPUT);
   serout.print("=(00 $ m)!");     	// Loop print values, see ColorPAL documentation
   serout.end();                   	// Discontinue serial port for transmitting
   pinMode(pin, INPUT);
-  serin.begin(4800);            	// Open serial port for Arduino to receive from ColorPAL
 }
 
 void ColorPAL::readPAL(int* r, int* g, int* b)
 {				
+ serin.begin(4800);            	// Open serial port for Arduino to receive from ColorPAL
+ delay(5);
  char buffer[32];
   if (serin.available() > 0)
   {
@@ -44,10 +44,9 @@ void ColorPAL::readPAL(int* r, int* g, int* b)
       }
       sscanf(buffer, "%3x%3x%3x", &red, &green, &blue);     // Parse the string in buffer for R, G, and B values
       *r=red; *g=green; *b=blue;
-	  Serial.print(red); Serial.print(" "); Serial.print(green); Serial.print(" "); Serial.println(blue);
-	  //delay(10);       // Set sensor refresh rate at 10Hz
     }
   }
+  serin.end();
 }
 
 int ColorPAL::redPAL()
@@ -91,4 +90,9 @@ void ColorPAL::resetPAL()
   delay(80);
   pinMode(pin, INPUT);
   delay(200);
+}
+
+void ColorPAL::detachPAL()
+{
+  serin.end();
 }
